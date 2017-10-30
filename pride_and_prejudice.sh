0eb22ef223abd445c8d62b44363630533d4fb6b9
Raphael Bulle <raphael.bulle@uni.lu>
2017-10-30 15:14:21 +0100

    Mistake : forget to change the .sh file in the previous commit, now it's done !

--- pride_and_prejudice.sh
+++ pride_and_prejudice.sh
@@ -1,3 +1,8 @@
 # Count the number of time the five sister's first name appear in Pride and prejudice
 
+for word in Elizabeth Jane Mary Catherine Lydia
+do
+	echo $word
+	grep -w -i $word Book.txt | wc -l
+done
 
